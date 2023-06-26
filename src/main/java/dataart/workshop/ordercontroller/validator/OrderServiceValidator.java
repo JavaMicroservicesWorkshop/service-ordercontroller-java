64a19dd08b10b9eabb7dd9567c9996a041c0a2b2
aprachov <andrii.prachov@gmail.com>
2023-06-26 18:18:23 +0200

    Minor fixes

--- src/main/java/dataart/workshop/ordercontroller/validator/OrderServiceValidator.java
+++ src/main/java/dataart/workshop/ordercontroller/validator/OrderServiceValidator.java
@@ -7,14 +7,13 @@ import org.springframework.stereotype.Component;
 
 @Component
 @RequiredArgsConstructor
-
 public class OrderServiceValidator {
 
     private final OrderRepository orderRepository;
 
     public void validateOrderPresence(String bookId) {
         if (!orderRepository.existsByOrderId(bookId)) {
-            throw new OrderNotFoundException("Can't find book by id: %s".formatted(bookId));
+            throw new OrderNotFoundException("Can't find order by id: %s".formatted(bookId));
         }
     }
 }
